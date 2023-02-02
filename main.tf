258eaf68a0c2200b8ae6b1651ec402cb0566fd0a
ankupsatpute <95556178+ankupsatpute@users.noreply.github.com>
2023-02-02 13:18:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "aws_security_group" "dev-security" {
 resource "aws_instance" "Demo" {
   ami                         = "ami-0cca134ec43cf708f"     // ami of amazon instance
   instance_type               = "t2.micro"
-  key_name                    = "MumbaiKey"                 
+  key_name                    = "LTIDEMO"                 
 
   subnet_id                   = "${aws_subnet.dev-public-1.id}"
   vpc_security_group_ids      = [aws_security_group.dev-security.id]
